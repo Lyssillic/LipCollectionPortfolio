807befe17ab18f245bd3d135ee0382795b9025ba
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2020-11-12 23:29:12 -0700

    Added app and model

---
+++ mysite/LipCollection/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LipcollectionConfig(AppConfig):
+    name = 'LipCollection'
---
+++ mysite/LipCollection/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class LipItem(models.Model):
+    def __str__(self):
+        return self.name
+
+    Name = models.CharField(max_length=200)
+    Brand = models.CharField(max_length=200)
+    Flavor = models.CharField(max_length=200)
+    Category = models.CharField(max_length=200)
+    Picture = models.ImageField(default='lipItem.PNG', upload_to='pictures')
+    Price = models.IntegerField()
+    Rarity = models.CharField(max_length=200)
