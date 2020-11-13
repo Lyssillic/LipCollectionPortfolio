607fc2a2f9131122c9825a0797d94a33572dc6d1
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2020-11-12 23:47:16 -0700

    Added to database:

--- mysite/LipCollection/models.py
+++ mysite/LipCollection/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class LipItem(models.Model):
     def __str__(self):
-        return self.name
+        return self.Name
 
     Name = models.CharField(max_length=200)
     Brand = models.CharField(max_length=200)
     Flavor = models.CharField(max_length=200)
     Category = models.CharField(max_length=200)
-    Picture = models.ImageField(default='lipItem.PNG', upload_to='pictures')
+    Picture = models.ImageField(default='lipItem.PNG', upload_to='media')
     Price = models.IntegerField()
     Rarity = models.CharField(max_length=200)
