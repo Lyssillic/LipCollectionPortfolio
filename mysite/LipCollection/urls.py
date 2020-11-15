33e8cbb686ead4a8c9ecf1a54e23965abfa64228
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2020-11-15 15:07:53 -0700

    Added pages and navbar links

--- mysite/LipCollection/urls.py
+++ mysite/LipCollection/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 app_name = 'LipCollection'
 
 urlpatterns = [
-    path('', views.about, name="about"),
+    path('home/', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('balm/', views.balm, name="balm"),
+    path('gloss/', views.gloss, name="gloss"),
+    path('scrub/', views.scrub, name="scrub"),
 ]
--- mysite/LipCollection/views.py
+++ mysite/LipCollection/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
 
+def home(request):
+    return render(request, 'LipCollection/home.html')
+
+
 def about(request):
     return render(request, 'LipCollection/about.html')
 
+
+def balm(request):
+    return render(request, 'LipCollection/balm.html')
+
+
+def gloss(request):
+    return render(request, 'LipCollection/gloss.html')
+
+
+def scrub(request):
+    return render(request, 'LipCollection/scrub.html')
